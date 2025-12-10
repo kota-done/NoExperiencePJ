4e2c62803254a7e99b73e12d633ba01865a205f0
kota-done <shinigami0706@gmail.com>
2025-12-10 19:29:38 +0900

    DB設定以外のビルド構築まで完了

---
+++ src/main/java/com/dev/raise/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.dev.raise.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dev.raise.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
